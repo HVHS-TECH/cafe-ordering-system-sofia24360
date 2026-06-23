754964a82c606afda4fa5c03250ae3f958186a35
sofia24360 <24360ss@hvhs.school.nz>
2026-06-23 00:18:03 +0000

    Jun 23, 2026, 12:18 AM

--- script.js
+++ script.js
@@ -12,9 +12,6 @@ console.log("Running ordering system");
  Main code
 **********************************/
 
-const OUTPUT = document.getElementById("spaceForJavaScriptOutput")
-const ITEM_FIELD = document.getElementById("itemField")
-const QUANTITY_FIELD = document.getElementById("quantityField")
 
 
 
@@ -23,26 +20,5 @@ const QUANTITY_FIELD = document.getElementById("quantityField")
 /***********************************
  Functions
 ***********************************/
-function getFormInput(){
-    let itemsArray = [ 
-    {
-        name:"Brownies",
-        price:12
-    },
-    {
-        name:"Egg Tart",
-        price:5
-    },
-    {
-        name:"Strawberry Cream Tart",
-        price:6
-    },
-    {
-        name:"Palmiers",
-        price:10
-    }
-    ]
-    let choice = ITEM_FIELD.value;
-    let qty = QUANTITY_FIELD.value;
-    OUTPUT.innerHTML += qty + "x " + itemsArray[choice];
+
 }
