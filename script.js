96a1830b1f911bb353b5bb21e138b4b457e3c753
sofia24360 <24360ss@hvhs.school.nz>
2026-06-24 02:34:55 +0000

    Jun 24, 2026, 2:34 AM

--- script.js
+++ script.js
@@ -25,5 +25,7 @@ const QUANTIY_FIELD = document.getElementById("quantityField")
 ***********************************/
 
 function getFormInput(){
-    let itemArray = ["Brownies", ""] 
+    let itemArray = ["Brownies", "Egg tart", "Strawberry Cream Tart", "Palmiers"];
+    let choice = ITEM_FIELD.value;
+    OUTPUT.innerHTML += "x " + itemArray[choice] + "<br>";
 }
