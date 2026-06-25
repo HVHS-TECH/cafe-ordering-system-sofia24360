45e93d44e9c3039a01d8bc7de05c450c8e9e3832
sofia24360 <24360ss@hvhs.school.nz>
2026-06-25 21:07:58 +0000

    Jun 25, 2026, 9:07 PM

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ console.log("Running ordering system");
 **********************************/
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 const ITEM_FIELD = document.getElementById("itemField");
-const QUANTITY_FIELD = document.getElementById("quantityField");
+const QTY_FIELD = document.getElementById("qtyField");
 
 
 
@@ -28,6 +28,6 @@ const QUANTITY_FIELD = document.getElementById("quantityField");
 function getFormInput(){
     let itemArray = ["Brownies", "Egg tart", "Strawberry Cream Tart", "Palmiers"];
     let choice = ITEM_FIELD.value;
-    let qty = QUANTITY_FIELD.value;
+    let qty = QTY_FIELD.value;
     OUTPUT.innerHTML += qty + "x " + itemArray[choice] + "<br>";
 }
