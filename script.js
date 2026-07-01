e7c087cf70ffee3ed5ad53bf626adee1fb096067
sofia24360 <24360ss@hvhs.school.nz>
2026-07-01 02:43:32 +0000

    Jul 1, 2026, 2:43 AM

--- script.js
+++ script.js
@@ -33,6 +33,8 @@ function getFormInput(){
     let qty = QTY_FIELD.value;
     OUTPUT.innerHTML += qty + "x " + itemArray[choice] + "<br>";
     calculatePrice(choice, qty)
+    let userorder[]
+    userorder.push 
     
 }
 
