5584414d48c88eaf179c8ecdf671efebe61bafd8
sofia24360 <24360ss@hvhs.school.nz>
2026-07-18 09:32:13 +0000

    Jul 18, 2026, 9:32 AM

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ getFormInput('Strawberry Cream Tart', 6)
 function getFormInput(itemName, itemPrice){
     let choice = {name:itemName, price:itemPrice}
     userOrder.push(choice)
-    getElementById("UserOrder").innerHTML += "<p> You have added " + choice.name + " $" + choice.price + "</p>";
+    getElementById("itemField").innerHTML += "<p> You have added " + choice.name + " $" + choice.price + "</p>";
 }
 
 
