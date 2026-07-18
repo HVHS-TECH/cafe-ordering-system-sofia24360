e3726fc5cb4f27b29eea15a857ca29573f075ce2
sofia24360 <24360ss@hvhs.school.nz>
2026-07-18 08:53:39 +0000

    Jul 18, 2026, 8:53 AM

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ const NAME_FIELD = document.getElementById("nameField");
 function getFormInput(itemName, itemPrice){
     let choice = {item:itemName, price:itemPrice}
     userorder.push(choice)
-    document.getElementById
+    document.getElementById("userOrder").innerHTML
 }
 
 
